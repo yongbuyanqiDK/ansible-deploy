69f9ce6300def734c413078119cb09921a0e369e
dongkai <dongkai@beyondcent.com>
2016-08-15 15:38:55 +0800

    ansible-deploy

--- jboss/roles/jboss/files/jboss-service.sh
+++ jboss/roles/jboss/files/jboss-service.sh
@@ -5,4 +5,11 @@ sstr=$(echo -e "\n")
 echo $sstr
 if -f /root/nohup.out; then
         tail -20 /root/nohup.out > /home/jboss.log
+else
+        nohup /usr/share/jboss/bin/standalone.sh &
+        sstr=$(echo -e "\n")
+        echo $sstr
+        if -f /root/nohup.out; then
+            tail -20 /root/nohup.out > /home/jboss.log
+        fi
 fi
