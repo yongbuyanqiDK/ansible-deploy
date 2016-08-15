9e3454b9a2e2a85b89f55683613abf4ef501f259
dongkai <dongkai@beyondcent.com>
2016-08-15 15:57:34 +0800

    ansible-deploy

--- mysql/roles/mysql/files/mysql.sh
+++ mysql/roles/mysql/files/mysql.sh
@@ -4,6 +4,6 @@ pwd=$(cat /root/.mysql_secret | awk -F '): ' '{print $2}')
 service mysql start
 mysqladmin -uroot -p$pwd password 123
 # 远程连接
-mysql -uroot -p123 mysql<test.sql
+mysql -uroot -p123 mysql</opt/test.sql
 #开机启动
 systemctl enable mysql.service
